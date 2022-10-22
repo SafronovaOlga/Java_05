b42da1f5bb37b1da59b3a640f395282c86fd3112
SafronovaOlga <olgasafronova.636531448@gmail.com>
2022-10-22 16:47:33 +0200

    In Utils class add a new method

--- src/main/java/project_utils/Utils.java
+++ src/main/java/project_utils/Utils.java
@@ -31,4 +31,24 @@ public class Utils {
         }
     }
 
+    public static int[] getMinMaxAverageArray(int[] array) {
+      int min = Integer.MAX_VALUE;
+      int max = Integer.MIN_VALUE;
+     // int average;
+      int sum = 0;
+
+      for(int i = 0; i < array.length; i++) {
+          if (array[i] < min) {
+              min = array[i];
+          }
+          if (array[i] > max) {
+              max = array[i];
+          }
+          sum += array[i];
+      }
+
+     // average = sum / array.length;
+        return new int[]{min, max, (sum / array.length)};
+    }
+
 }
